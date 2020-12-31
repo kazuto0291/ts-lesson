350c5028ed8e55986dd7421974774e774d63f925
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-31 19:40:41 +0900

    tuple型
    TypeScript独自の型：長さの固定の配列.
    pushが許可されるのには注意する。

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var person = {
     name: 'yota',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+person.role[1] = 10;
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
