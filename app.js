d4fa2a6d66c615f5f2b8d0d379614aa3907ce4c9
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-31 21:38:43 +0900

    関数の引数と戻り値の型

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    // 型がわからないのを足すことをtypescriptはエラーを出すので
-    // ランタイム上でデータ型をチェックすることで解消できる（typeofを使う）
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
