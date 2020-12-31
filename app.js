89f2939e85ee9cafad92caf2cb099080c2075490
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-31 20:39:00 +0900

    Literal型
    型を固定した値にする

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     // 型がわからないのを足すことをtypescriptはエラーを出すので
     // ランタイム上でデータ型をチェックすることで解消できる（typeofを使う）
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,17 +1,20 @@
-function combine(input1:number | string , input2: number | string) {
+function combine(input1:number | string , input2: number | string, resultConversion: 'as-number' | 'as-text') {
   let result;
   // 型がわからないのを足すことをtypescriptはエラーを出すので
   // ランタイム上でデータ型をチェックすることで解消できる（typeofを使う）
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2
   } else {
     result = input1.toString() + input2.toString();
   }
-    return result;
+  return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Max', 'Anna');
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
