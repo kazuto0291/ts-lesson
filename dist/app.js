a3c4038f96f9bdf958ea3fbdedeb99a936d4a2ac
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-01 13:57:03 +0900

    クラスの継承

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,18 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// クラスのプロパティとはクラスの変数です。
 var Department = /** @class */ (function () {
     function Department(id, name) {
         this.id = id;
@@ -10,7 +24,7 @@ var Department = /** @class */ (function () {
         // this.name = n;
     }
     Department.prototype.describe = function () {
-        console.log("Department: (" + this.id + ": " + this.name);
+        console.log("Department: (" + this.id + "): " + this.name);
     };
     Department.prototype.addEmployee = function (employee) {
         this.employees.push(employee);
@@ -21,10 +35,40 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department('d1', 'Accounting');
-console.log(accounting);
-accounting.addEmployee('Max');
-accounting.addEmployee('Aun');
-accounting.printEmployeeInformation();
-accounting.describe();
+// 継承
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT') || this;
+        _this.admins = admins; //コンストラクターに定義しているので省略できる
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var it = new ITDepartment('d1', ['Max']);
+console.log(it);
+it.addEmployee('Max');
+it.addEmployee('Aun');
+console.log(it, 'it');
+it.printEmployeeInformation();
+it.describe();
+//継承
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something');
+accounting.printReports();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// クラスのプロパティとはクラスの変数です。
 class Department {
   // private readonly id: string;//readonlyは値を変更できない
   // name: string;
@@ -9,7 +10,7 @@ class Department {
   }
 
   describe(this: Department) {
-    console.log(`Department: (${this.id}: ${this.name}`)
+    console.log(`Department: (${this.id}): ${this.name}`)
   }
 
   addEmployee(employee: string) {
@@ -22,13 +23,42 @@ class Department {
   }
 }
 
-const accounting = new Department('d1','Accounting');
-console.log(accounting);
+// 継承
+class ITDepartment extends Department {
+  admins: string[];//コンストラクターに定義しているので省略できる
+  constructor(id: string,  admins: string[]) {
+    super(id, 'IT');//コンストラクターの情報を継承元のクラスのコンストラクターに値を渡すためにsuperを使う
+    this.admins = admins;//コンストラクターに定義しているので省略できる
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Aun');
+const it = new ITDepartment('d1',['Max']);
+console.log(it);
 
+it.addEmployee('Max');
+it.addEmployee('Aun');
 
-accounting.printEmployeeInformation();
+console.log(it, 'it')
 
-accounting.describe();
+it.printEmployeeInformation();
+
+it.describe();
+
+//継承
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports)
+  }
+
+}
+
+const accounting = new AccountingDepartment('d2',[]);
+accounting.addReport('Something');
+accounting.printReports();
