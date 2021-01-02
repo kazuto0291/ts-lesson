0f45b90c1c55218c44efd25b7350165ceec63385
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-02 19:11:37 +0900

    交差型
    複数のオブジェクトの型を一つのオブジェクトの型にできる。union型の場合は共通の部分が適用される

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
+var e1 = {
+    name: 'Mac',
+    privileges: ['create-server'],
+    startDate: new Date(),
+};
+var e2 = {
+    name: 'Mac',
+    privileges: ['create-server'],
+    startDate: new Date(),
+};
 //# sourceMappingURL=app.js.map
