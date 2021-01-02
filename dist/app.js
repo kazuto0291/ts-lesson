e1696a489936a7483b9c173b998960ea4067a2a1
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-02 18:25:37 +0900

    インターフェース
    オブジェクトの型の定義

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,24 @@
 "use strict";
+// インターフェースには構造だけ設定できる
+// インターフェースはオブジェクトの型を定義するため
+// インターフェースは実際の実装内容や具体的な値を持つことできない、
+// インターフェースを実装するクラスがそれがどういう機能持っているかという構造を定義できる。
+var Person = /** @class */ (function () {
+    function Person(n, age) {
+        this.name = n;
+        this.age = age;
+    }
+    Person.prototype.greet = function (phrase) {
+        console.log(phrase + ' ' + this.name);
+    };
+    return Person;
+}());
+var user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet: function (phrase) {
+        console.log(phrase + '' + this.name);
+    },
+};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,32 @@
 // インターフェースには構造だけ設定できる
-interface Person {
-  name:string;
-  age:number;
+// インターフェースはオブジェクトの型を定義するため
+// インターフェースは実際の実装内容や具体的な値を持つことできない、
+// インターフェースを実装するクラスがそれがどういう機能持っているかという構造を定義できる。
+
+interface Greetable {
+  readonly name:string;
+  greet(phrase: string): void;
 }
+
+class Person implements Greetable {
+  name: string;
+  age :number;
+  constructor(n: string, age: number) {
+    this.name = n;
+    this.age = age
+  }
+  greet(phrase:string) {
+    console.log(phrase + ' ' + this.name)
+  }
+}
+
+
+let user1: Person;
+
+user1 = {
+  name: 'Max',
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + '' + this.name);
+  },
+};
