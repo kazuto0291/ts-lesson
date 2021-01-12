abe4466ad2e0c62289acaba1da93b08485d1c67a
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-13 01:05:48 +0900

    function-overlaod

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,12 @@
 "use strict";
-var errorBag = {
-    email: '正しいメールアドレスではありません。',
-    username: 'ユーザ名に記号を含めることはできません。'
-};
+// functionオーバロード
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+var result3 = add(1, 5);
+var result2 = add('Hello', 'TypeScript');
+result2.split('');
 //# sourceMappingURL=app.js.map
