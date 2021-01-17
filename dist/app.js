6700316c556edba066c2d5f426b3631014d29223
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-17 19:49:48 +0900

    デコレーターを追加できる場所５つ
    ・classデコレーター
    ・propertyデコレーター
    ・Accessorデコレーター
    ・Methodデコレーター
    ・parameterデコレーター

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // デコレータをカスタマイズする
 function Logger(logString) {
     return function (constructor) {
@@ -17,15 +20,85 @@ function Logger(logString) {
     };
 }
 // デコレータ関数を実行する関数を返す
-let Person1 = class Person1 {
+// @Logger("ログ出力 - PERSON")
+// class Person1 {
+//   name = 'Max';
+//   constructor() {
+//     console.log("Personオブジェクトを作成中...");
+//   }
+// }
+// const pers = new Person1();
+// console.log(pers);
+// ======================
+function WithTemplate(template, hookId) {
+    return function (_) {
+        console.log('テンプレートを表示');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+        }
+    };
+}
+let Person3 = class Person3 {
     constructor() {
-        this.name = 'Max';
-        console.log("Personオブジェクトを作成中...");
+        this.name = "masaya";
+        console.log("Person３オブジェクトを作成中");
     }
 };
-Person1 = __decorate([
-    Logger("ログ出力 - PERSON")
-], Person1);
-const pers = new Person1();
-console.log(pers);
+Person3 = __decorate([
+    Logger("ログ出力 - PERSON"),
+    WithTemplate("<h1>Personオブジェクト</h1>", "app")
+], Person3);
+const pers3 = new Person3();
+console.log(pers3);
+// ーーーーーーー
+function Log(target, propertyName) {
+    console.log("Property デコレーター");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Accessor デコレーター");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log("Method デコレーター");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log("parameter デコレーター");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('不正な価格です。- 0以下は設定できません');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,89 @@ function Logger(logString: string) {
   }
 }
 // デコレータ関数を実行する関数を返す
-@Logger("ログ出力 - PERSON")
-class Person1 {
-  name = 'Max';
+// @Logger("ログ出力 - PERSON")
+// class Person1 {
+//   name = 'Max';
   
+//   constructor() {
+//     console.log("Personオブジェクトを作成中...");
+//   }
+// }
+
+// const pers = new Person1();
+
+// console.log(pers);
+// ======================
+
+function WithTemplate(template: string, hookId: string) {
+  return function(_: Function) { //アンダースコアは引数を受け取るけど使わないことを示す
+    console.log('テンプレートを表示')
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  }
+}
+@Logger("ログ出力 - PERSON")
+@WithTemplate("<h1>Personオブジェクト</h1>", "app")
+class Person3 {
+  name = "masaya"
   constructor() {
-    console.log("Personオブジェクトを作成中...");
+    console.log("Person３オブジェクトを作成中")
   }
 }
 
-const pers = new Person1();
+const pers3 = new Person3();
 
-console.log(pers);
+console.log(pers3);
+
+// ーーーーーーー
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property デコレーター");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name:string, descriptor: PropertyDescriptor) {
+  console.log("Accessor デコレーター");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string| Symbol, descriptor: PropertyDescriptor) {
+    console.log("Method デコレーター");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number ) {
+  console.log("parameter デコレーター");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('不正な価格です。- 0以下は設定できません')
+    }
+  }
+
+  constructor(t:string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * ( 1 + tax);
+  }
+}
