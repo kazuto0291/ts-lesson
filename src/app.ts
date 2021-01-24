3298a60df6c3b04d48e2f54744ded6c1678d53c4
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-24 21:55:55 +0900

    getter関数を利用して取得したデータの値を変更して表示する

--- src/app.ts
+++ src/app.ts
@@ -162,6 +162,15 @@ function autobind(
 // 一つ一つのプロジェクトをリストの項目として表示する為のクラス
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  get manday() {
+    if (this.project.manday < 20) {
+      return this.project.manday.toString() + '人日';
+    } else {
+      return (this.project.manday / 20).toString() + '人月';
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -173,7 +182,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.manday.toString();
+    this.element.querySelector('h3')!.textContent = this.manday; //getter関数が実行される
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
